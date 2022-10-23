e64e182e6a27f75df32f19e0468572d1d4378085
Camilla-Marques <camilla.93marques@gmail.com>
2022-10-23 18:05:05 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Camilla da Conceição Marques on 23/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
